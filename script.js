8a97abc3a6024a8efdc48161ea472bcf7192e479
satishcoderZZ1 <167805933+satishcoderZZ1@users.noreply.github.com>
2025-05-29 20:57:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
+	const extended = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
   //your code here
 	let result='';
